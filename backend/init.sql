6b74d4129c4868feb8e2e6d29bb2e8f9669b129e
aryanchoudharyyyy <aryanchoudhary.9289@gmail.com>
2026-08-22 11:03:44 +0530

    feat(database): add init script for automatic Postgres DB creation

---
+++ backend/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE squadstation_users;
+CREATE DATABASE squadstation_trips;
+CREATE DATABASE squadstation_groups;
+CREATE DATABASE squadstation_chat;
+CREATE DATABASE squadstation_marketplace;
